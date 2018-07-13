723de45ee476b2ed2beb26e5ed8603e7b5ae35be
DeepBass <danbass8@googlemail.com>
2018-07-13 19:51:38 +0100

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+var answer = '';
+
+// Beginners coding club javascript exercise
+
+// The variable at the top is the answer variable. 
+// Try setting it to 4 to answer the question successfully
+
+
+// This line of code puts your answer into the page. This process will be covered later.
+document.getElementById('answer').innerHTML = answer;
